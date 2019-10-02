9b3c26e950aa86ae1eb5d294ad6fe22f91ab137a
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2019-10-02 13:38:56 +0200

    Add examples of using test harness and integration test

--- src/test/com/ververica/learnflink/function/FraudDetectorTest.java
+++ src/test/com/ververica/learnflink/function/FraudDetectorTest.java
@@ -3,6 +3,7 @@ package com.ververica.learnflink.function;
 import com.ververica.learnflink.entity.FraudAlert;
 import com.ververica.learnflink.entity.Transaction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
 import org.apache.flink.streaming.api.operators.KeyedProcessOperator;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
@@ -76,7 +77,11 @@ public class FraudDetectorTest {
                         Transaction::getAccountId,
                         TypeInformation.of(Long.class)
                 );
+
+//        testHarness.setup();
+//        testHarness.initializeEmptyState();
         testHarness.open();
+
 
         testHarness.setProcessingTime(0L);
         testHarness.processWatermark(0L);
@@ -93,6 +98,8 @@ public class FraudDetectorTest {
 
         testHarness.setProcessingTime(60 * 1000 - 1);
         testHarness.processWatermark(60 * 1000 - 1);
+
+        OperatorSubtaskState snapshot = testHarness.snapshot(0L, 60 * 1000 - 1);
 
         assertThat(testHarness.numKeyedStateEntries(), is(2));
         assertThat(testHarness.numProcessingTimeTimers(), is(1));
@@ -111,6 +118,38 @@ public class FraudDetectorTest {
 
         testHarness.processWatermark(60 * 1000);
 
+        testHarness.close();
+        // recreate testHarness, this is necessary because after restore, Flink uses another instance of the operator
+        // otherwise, one gets error on initialization:
+        // java.lang.IllegalStateException: TestHarness has already been initialized. Have you opened this harness before initializing it?
+        testHarness =
+                new KeyedOneInputStreamOperatorTestHarness<>(
+                        operator,
+                        Transaction::getAccountId,
+                        TypeInformation.of(Long.class)
+                );
+        testHarness.setup();
+        testHarness.initializeState(snapshot);
+        testHarness.open();
+
+
+        assertThat(testHarness.numKeyedStateEntries(), is(2));
+        assertThat(testHarness.numProcessingTimeTimers(), is(1));
+        assertThat(testHarness.numEventTimeTimers(), is(0));
+
+        // check operator state
+        //assertThat(f.getFoundState().value(), is(true));
+        //assertThat(f.getTimerState().value(), is(60000L));
+
+        testHarness.processElement(t2, 1L);
+
+        assertThat(testHarness.numKeyedStateEntries(), is(0));
+        assertThat(testHarness.numProcessingTimeTimers(), is(0));
+        assertThat(testHarness.numEventTimeTimers(), is(0));
+
+        testHarness.processWatermark(60 * 1000);
+
+
         //System.out.println("Processing Time: " + testHarness.getProcessingTime() );
         //System.out.println(testHarness.getOutput());
 
@@ -118,8 +157,8 @@ public class FraudDetectorTest {
         alter.setId(0);
 
         ConcurrentLinkedQueue<Object> expectedOutput = new ConcurrentLinkedQueue<>();
-        expectedOutput.add(new Watermark(0L));
-        expectedOutput.add(new Watermark(60000L-1));
+        //expectedOutput.add(new Watermark(0L));
+        //expectedOutput.add(new Watermark(60000L-1));
         expectedOutput.add(new StreamRecord<>(alter, 1L));
         expectedOutput.add(new Watermark(60000L));
         TestHarnessUtil.assertOutputEquals( "Unexpected output", expectedOutput, testHarness.getOutput());
