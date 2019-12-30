f3286fafe20bc380e0f57cc9f298ce2a54ec5fd2
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2019-12-30 11:34:06 +0100

    Implement two WordCount(Custom)StateReadFunction: ValueState<Integer> and ValueState<String,Integer>

--- src/main/java/com/ververica/learnflink/function/WordCountCustomStateReadFunction.java
+++ src/main/java/com/ververica/learnflink/function/WordCountCustomStateReadFunction.java
@@ -2,22 +2,19 @@ package com.ververica.learnflink.function;
 
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.state.api.functions.KeyedStateReaderFunction;
 import org.apache.flink.util.Collector;
 
-public class WordCountStateReadFunction extends KeyedStateReaderFunction<String, Tuple2<String,Integer>> {
+public class WordCountCustomStateReadFunction extends KeyedStateReaderFunction<String, Tuple2<String,Integer>> {
 
-    ValueState<Tuple2<String, Integer>> state;
+    ValueState<Integer> state;
 
     @Override
     public void open(Configuration parameters) {
-        ValueStateDescriptor<Tuple2<String,Integer>> stateDescriptor = new ValueStateDescriptor<>("_op_state",
-                TypeInformation.of(new TypeHint<Tuple2<String, Integer>>(){})
-        );
+        ValueStateDescriptor<Integer> stateDescriptor = new ValueStateDescriptor<>("custom", Types.INT);
         state = getRuntimeContext().getState(stateDescriptor);
 
     }
@@ -28,6 +25,9 @@ public class WordCountStateReadFunction extends KeyedStateReaderFunction<String,
             Context ctx,
             Collector<Tuple2<String,Integer>> out) throws Exception {
 
-        out.collect(state.value());
+        Tuple2<String,Integer> data = new Tuple2<String,Integer>();
+        data.f0 = key;
+        data.f1 = state.value();
+        out.collect(data);
     }
 }
