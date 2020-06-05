ba814b2e10c089bd2212e1353dafb553627fbb3a
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-06-05 15:14:19 +0200

    Add sample code

--- src/main/java/com/ververica/learnflink/BatchJob.java
+++ src/main/java/com/ververica/learnflink/BatchJob.java
@@ -18,7 +18,11 @@
 
 package com.ververica.learnflink;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+
+import java.util.Arrays;
 
 /**
  * Skeleton for a Flink Batch Job.
@@ -36,31 +40,9 @@ public class BatchJob {
 		// set up the batch execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataSet<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/batch/index.html
-		 *
-		 * and the examples
-		 *
-		 * http://flink.apache.org/docs/latest/apis/batch/examples.html
-		 *
-		 */
-
-		// execute program
-		env.execute("Flink Batch Java API Skeleton");
+		DataSet<String> data = env.fromCollection(Arrays.asList("a", "b"));
+		data.filter((FilterFunction<String>) value -> value.startsWith("a"))
+				.printOnTaskManager("prefix");
+		env.execute("flink-test-batch-job");
 	}
 }
