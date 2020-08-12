d476456b05b9137c1868a75976c22ea9ccacee4a
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-08-12 13:40:30 +0200

    comment out all

--- src/main/java/com/ververica/learnflink/TableJob.java
+++ src/main/java/com/ververica/learnflink/TableJob.java
@@ -1,27 +1,22 @@
 package com.ververica.learnflink;
 
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.java.StreamTableEnvironment;
-import org.apache.flink.table.descriptors.FileSystem;
-import org.apache.flink.table.descriptors.Schema;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
+//import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+//import org.apache.flink.table.api.DataTypes;
+//import org.apache.flink.table.api.EnvironmentSettings;
+//import org.apache.flink.table.api.java.StreamTableEnvironment;
+//import org.apache.flink.table.descriptors.Schema;
 
 
 public class TableJob {
     public static void main(String[] args) {
-        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().useOldPlanner().inStreamingMode().build();
-        StreamExecutionEnvironment fsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(fsEnv, fsSettings);
-
-        final Schema schema = new Schema()
-                .field("a", DataTypes.INT())
-                .field("b", DataTypes.STRING())
-                .field("c", DataTypes.FLOAT());
+//        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().useOldPlanner().inStreamingMode().build();
+//        StreamExecutionEnvironment fsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+//        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(fsEnv, fsSettings);
+//
+//        final Schema schema = new Schema()
+//                .field("a", DataTypes.INT())
+//                .field("b", DataTypes.STRING())
+//                .field("c", DataTypes.FLOAT());
 
 
 //        DataStream<Tuple2<Long, String>> stream = ...
